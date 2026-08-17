85cede932a2468ac50f2097fb0b5646bb5d8893b
Laurence Floriano Luciano <laurence.55925@alunosatc.edu.br>
2026-08-17 20:49:07 -0300

    Implementar código do exercicio 1, 2 e 3 apenas.

---
+++ src/main/java/contas/ContaEstrangeira.java
@@ -0,0 +1,19 @@
+package contas;
+
+
+public class ContaEstrangeira extends Conta {
+    private static final double COTACAO_DOLAR = 5.40;
+
+
+    public ContaEstrangeira(String titular, String numero) {
+        super(titular, numero);
+    }
+
+    public double getSaldoEmReais() {
+        return COTACAO_DOLAR * this.getSaldo();
+    }
+
+    public String tipoDeConta() {
+        return "Conta em Dólar";
+    }
+}
